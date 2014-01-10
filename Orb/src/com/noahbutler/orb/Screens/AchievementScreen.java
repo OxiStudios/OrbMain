e9ba3ea47eadcb359863c144e405d6f6b5297bfa
liddictm <liddictm@uwec.edu>
2014-01-09 19:19:47 -0600

    AchievementScreen Updated with >= and corrected each method.

--- Orb/src/com/noahbutler/orb/Screens/AchievementScreen.java
+++ Orb/src/com/noahbutler/orb/Screens/AchievementScreen.java
@@ -33,84 +33,109 @@ public class AchievementScreen extends MasterScreen {
 
 	public boolean kill_Killionare_goal() {
 		if (OrbGame.saveFile.kills_made >= 500) {
-			return OrbGame.saveFile.kill_Killionare;
+			OrbGame.saveFile.kill_Killionare = true;
+		} else {
+			OrbGame.saveFile.kill_Killionare = false;
 		}
 		return OrbGame.saveFile.kill_Killionare;
 	}
 
 	public boolean kill_OohKillEm_goal() {
 		if (OrbGame.saveFile.kills_made >= 1000) {
-			return OrbGame.saveFile.kill_OohKillEm;
+			OrbGame.saveFile.kill_OohKillEm = true;
+		} else {
+			OrbGame.saveFile.kill_OohKillEm = false;
 		}
 		return OrbGame.saveFile.kill_OohKillEm;
 	}
 
 	public boolean level_TheNewGuy_goal() {
-		if (OrbGame.saveFile.level_reached == 5) {
-			return OrbGame.saveFile.level_TheNewGuy;
+		if (OrbGame.saveFile.level_reached >= 5) {
+			OrbGame.saveFile.level_TheNewGuy = true;
+		} else {
+			OrbGame.saveFile.level_TheNewGuy = false;
 		}
 		return OrbGame.saveFile.level_TheNewGuy;
 	}
 
 	public boolean level_SoISeeYouLikeToKillThings_goal() {
-		if (OrbGame.saveFile.level_reached == 10) {
-			return OrbGame.saveFile.level_SoISeeYouLikeToKillThings;
+		if (OrbGame.saveFile.level_reached >= 10) {
+			OrbGame.saveFile.level_SoISeeYouLikeToKillThings = true;
+		} else {
+			OrbGame.saveFile.level_SoISeeYouLikeToKillThings = false;
 		}
 		return OrbGame.saveFile.level_SoISeeYouLikeToKillThings;
 	}
 
 	public boolean boss_DefeatBoss1_goal() {
-		if (OrbGame.saveFile.boss_reached == 1) {
-			return OrbGame.saveFile.boss_DefeatBoss1;
+		if (OrbGame.saveFile.boss_reached >= 1) {
+			OrbGame.saveFile.boss_DefeatBoss1 = true;
+			;
+		} else {
+			OrbGame.saveFile.boss_DefeatBoss1 = false;
 		}
 		return OrbGame.saveFile.boss_DefeatBoss1;
 	}
 
 	public boolean boss_DefeatBoss2_goal() {
-		if (OrbGame.saveFile.boss_reached == 2) {
-			return OrbGame.saveFile.boss_DefeatBoss2;
+		if (OrbGame.saveFile.boss_reached >= 2) {
+			OrbGame.saveFile.boss_DefeatBoss2 = true;
+		} else {
+			OrbGame.saveFile.boss_DefeatBoss2 = false;
 		}
 		return OrbGame.saveFile.boss_DefeatBoss2;
 	}
 
 	public boolean boss_DefeatBoss3_goal() {
-		if (OrbGame.saveFile.boss_reached == 3) {
-			return OrbGame.saveFile.boss_DefeatBoss3;
+		if (OrbGame.saveFile.boss_reached >= 3) {
+			OrbGame.saveFile.boss_DefeatBoss3 = true;
+		} else {
+			OrbGame.saveFile.boss_DefeatBoss3 = false;
 		}
 		return OrbGame.saveFile.boss_DefeatBoss3;
 	}
 
 	public boolean boss_DefeatBoss4_goal() {
-		if (OrbGame.saveFile.boss_reached == 4) {
-			return OrbGame.saveFile.boss_DefeatBoss4;
+		if (OrbGame.saveFile.boss_reached >= 4) {
+			OrbGame.saveFile.boss_DefeatBoss4 = true;
+		} else {
+			OrbGame.saveFile.boss_DefeatBoss4 = false;
 		}
 		return OrbGame.saveFile.boss_DefeatBoss4;
 	}
 
 	public boolean play_YouPlayedHowLong_goal() {
-		if (OrbGame.saveFile.play_time == 10) {
-			return OrbGame.saveFile.play_YouPlayedHowLong;
+		if (OrbGame.saveFile.play_time >= 10) {
+			OrbGame.saveFile.play_YouPlayedHowLong = true;
+		} else {
+			OrbGame.saveFile.play_YouPlayedHowLong = false;
 		}
 		return OrbGame.saveFile.play_YouPlayedHowLong;
 	}
 
 	public boolean play_NotRPG_goal() {
-		if (OrbGame.saveFile.play_time == 25) {
-			return OrbGame.saveFile.play_NotRPG;
+		if (OrbGame.saveFile.play_time >= 25) {
+			OrbGame.saveFile.play_NotRPG = true;
+		} else {
+			OrbGame.saveFile.play_NotRPG = false;
 		}
 		return OrbGame.saveFile.play_NotRPG;
 	}
 
 	public boolean area_IntoTheVoid_goal() {
-		if (OrbGame.saveFile.area_unlocked == 1) {
-			return OrbGame.saveFile.area_IntoTheVoid;
+		if (OrbGame.saveFile.area_unlocked >= 1) {
+			OrbGame.saveFile.area_IntoTheVoid = true;
+		} else {
+			OrbGame.saveFile.area_IntoTheVoid = false;
 		}
 		return OrbGame.saveFile.area_IntoTheVoid;
 	}
 
 	public boolean area_TheyCameFromWithin_goal() {
-		if (OrbGame.saveFile.area_unlocked == 2) {
-			return OrbGame.saveFile.area_TheyCameFromWithin;
+		if (OrbGame.saveFile.area_unlocked >= 2) {
+			OrbGame.saveFile.area_TheyCameFromWithin = true;
+		} else {
+			OrbGame.saveFile.area_TheyCameFromWithin = false;
 		}
 		return OrbGame.saveFile.area_TheyCameFromWithin;
 	}
