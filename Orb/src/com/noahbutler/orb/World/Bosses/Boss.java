b55662ab6c5aceeda43077ab3b7d33444cd12054
Noah Butler <butlernc@uwec.edu>
2014-01-29 00:05:24 -0600

    added boss shit
    starting the boss class

---
+++ Orb/src/com/noahbutler/orb/World/Bosses/Boss.java
@@ -0,0 +1,19 @@
+package com.noahbutler.orb.World.Bosses;
+
+import com.noahbutler.orb.World.Entity;
+
+public class Boss extends Entity{
+	
+	int health;
+	int shield;
+	int weakness;
+	
+	public Boss() {
+		
+	}
+	
+	public void render() {
+		
+	}
+
+}
