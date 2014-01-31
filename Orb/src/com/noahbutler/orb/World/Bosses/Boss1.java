0e1fd8b400059709b6eb6715050260f9fe000a7d
liddictm <liddictm@uwec.edu>
2014-01-31 17:22:26 -0600

    Updated Boss Stuff. I originally made it so we would have different classes for different bosses. But in class you said it would repeat a lot of code and I agree. We can change it to whatever you want.

--- Orb/src/com/noahbutler/orb/World/Bosses/Boss1.java
+++ Orb/src/com/noahbutler/orb/World/Bosses/Boss1.java
@@ -4,21 +4,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.noahbutler.orb.OrbGame;
 import com.noahbutler.orb.World.Entity;
 import com.noahbutler.orb.World.World;
 
-public class Boss1 extends Entity{
+public class Boss1 extends Entity {
 	private int type;
 	private boolean canBeDeleted;
 	private int health;
-	
+
 	private Texture texture;
-	
+
 	private int shield;
 	private int weakness;
 	private World world;
 	public Sprite sprite;
-	
+
 	public Boss1(int type, World world) {
 		this.type = type;
 		this.world = world;
@@ -26,16 +27,64 @@ public class Boss1 extends Entity{
 		texture = new Texture(Gdx.files.internal(""));
 		sprite = new Sprite(texture);
 	}
-	
+
 	public void render(SpriteBatch b) {
-		sprite.setPosition(getPosition().x- sprite.getWidth()/2, getPosition().y- sprite.getHeight()/2);
+		sprite.setPosition(getPosition().x - sprite.getWidth() / 2,
+				getPosition().y - sprite.getHeight() / 2);
 		sprite.draw(b);
 	}
-	
-	public int getType(){
+
+	public int getType() {
 		return this.type;
 	}
-	
-	
 
+	public void bulletToBoss(String isBullet) {
+		try {
+			int BulletNum = Integer.parseInt(isBullet);
+			if (BulletNum >= 1) {
+				health -= 90;
+			}
+			if (BulletNum >= 2) {
+				health -= 80;
+			}
+			if (BulletNum >= 3) {
+				health -= 70;
+			}
+			if (BulletNum >= 4) {
+				health -= 60;
+			}
+			if (BulletNum >= 5) {
+				health -= 50;
+			}
+			if (BulletNum >= 6) {
+				health -= 40;
+			}
+			if (BulletNum >= 7) {
+				health -= 30;
+			}
+			if (BulletNum >= 8) {
+				health -= 20;
+			}
+			if (BulletNum >= 9) {
+				health -= 10;
+			} else {
+				health -= 100;
+			}
+			if (health <= 0) {
+				OrbGame.saveFile.score += 1;
+				Gdx.app.log("Score", "Score: " + OrbGame.saveFile.score);
+			}
+			Gdx.app.log("Boss Killed", "You have defeated the boss");
+		} catch (NumberFormatException nfe) {
+			// not a bullet, ground, or ship
+		}
+
+	}
+
+	public String getCheckDeletable() {
+		if (health <= 0) {
+			return "yes";
+		}
+		return "no";
+	}
 }
