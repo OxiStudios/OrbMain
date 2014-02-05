587941c63d252e2032730a9b4b04a8e2c9d4bcf6
Noah Butler <butlernc@uwec.edu>
2014-02-05 12:51:58 -0600

    bullet shit
    just added some more stuff to the bullet classes and reworking the way
    the physics will be created for them. It will the same way the physics
    are created for the abilities

---
+++ Orb/src/com/noahbutler/orb/World/Ship/Bullets/BulletPhysicsCreator.java
@@ -0,0 +1,15 @@
+package com.noahbutler.orb.World.Ship.Bullets;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class BulletPhysicsCreator {
+	
+	public BulletPhysicsCreator() {
+		
+	}
+	
+	public void bulletSmall(Vector2 position, Bullet bullet) {
+		
+	}
+
+}
